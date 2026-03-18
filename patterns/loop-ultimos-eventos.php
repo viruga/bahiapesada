09ece2c4a48eb02a6b0f3944766447152a45a1c8
Wesley DA COSTA SOUSA ROCHA EXT <wesley_da_costa_sousa_rocha_ext@carrefour.com>
2026-03-18 09:07:41 -0300

    correção de link

--- patterns/loop-ultimos-eventos.php
+++ patterns/loop-ultimos-eventos.php
@@ -30,7 +30,7 @@ $args = array(
 $query = new WP_Query($args);
 echo '<div id="eventos" class="mt-5 py-5 eventos-home" style="background: #232323;"><div class="container">';
 if ($query->have_posts()) {
-    echo '<h2 class="mb-4 d-flex justify-content-between align-items-end">Próximos eventos na Bahia <small><a href="https://bahiapesada.com.br/?post_type=eventos">Todos os eventos</a></small></h2>';
+    echo '<h2 class="mb-4 d-flex justify-content-between align-items-end">Próximos eventos na Bahia <small class="fs-6 fw-normal d-inline-block w-50 text-end"><a href="https://bahiapesada.com.br/?post_type=eventos">Todos os eventos</a></small></h2>';
     echo '<div class="row gx-md-4 gx-3">';
     while ($query->have_posts()) {
         $query->the_post();
